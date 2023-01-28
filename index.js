c12219603091e1088d8e742789c2c5072838b237
ravi848101 <106373902+ravi848101@users.noreply.github.com>
2023-01-28 19:33:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+
+// var video = document.getElementById("myVideo");
+
+
+
+// var btn = document.getElementById("myBtn");
+
+
+// function myFunction() {
+//     if (video.paused) {
+//         video.play();
+//         btn.innerHTML = "Pause";
+//     } else {
+//         video.pause();
+//         btn.innerHTML = "Play";
+//     }
+// }
+
+// $(document).ready(function () {
+
+//     $(".toggle-accordion").on("click", function () {
+//         var accordionId = $(this).attr("accordion-id"),
+//             numPanelOpen = $(accordionId + ' .collapse.in').length;
+
+//         $(this).toggleClass("active");
+
+//         if (numPanelOpen == 0) {
+//             openAllPanels(accordionId);
+//         } else {
+//             closeAllPanels(accordionId);
+//         }
+//     })
+
+//     openAllPanels = function (aId) {
+//         console.log("setAllPanelOpen");
+//         $(aId + ' .panel-collapse:not(".in")').collapse('show');
+//     }
+//     closeAllPanels = function (aId) {
+//         console.log("setAllPanelclose");
+//         $(aId + ' .panel-collapse.in').collapse('hide');
+//     }
+
+// });
+
+AOS.init({
+    duration: 1200,
+    delay: 500
+});
+
+
+
+
+
+
+
+
+// VANTA.WAVES({
+//     el: ".domain_name",
+//     mouseControls: true,
+//     touchControls: true,
+//     gyroControls: false,
+//     minHeight: 200.00,
+//     minWidth: 200.00,
+//     scale: 1.00,
+//     scaleMobile: 1.00,
+//     color: 0x3e83,
+//     shininess: 40.00,
+//     waveSpeed: 1.20
+// })
+
+
